fd32923f5a6a469fd743699ce6de11ab5de0bf87
Aisalkyn2023 <aysalkyn_ktum@mail.ru>
2023-01-05 23:13:56 +0600

    add home-work 2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
 
+const obj = {
+  logo: 'https://www.edigitalagency.com.au/wp-content/uploads/Netflix-logo-red-black-png.png',
+  btns: {
+    sel: {
+      opt1: 'Russian',
+      opt2: 'ENglish'
+    },
+    btn: 'Войти'
+  }
+}
+
 function App() {
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img className="logo" src={obj.logo} alt='img' />
+      <div className='btns'>
+        <select className='lang-btn'>
+          <option>{obj.btns.sel.opt1}</option>
+          <option>{obj.btns.sel.opt2}</option>
+        </select>
+        <button className='header-btn'>{obj.btns.btn}</button>
+      </div>
     </div>
   );
 }
